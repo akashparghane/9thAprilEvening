0ccad59665aa9b0337f302ed75c771e06c65cb28
Akash Parghane <parghaneakash999@gmail.com>
2022-08-16 21:04:04 +0530

    modified files

--- src/PNP/Combined.java
+++ src/PNP/Combined.java
@@ -4,7 +4,7 @@ public class Combined {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- 
+   // changes apply for gits
 		String name;
 		byte rollnumber;
 		int age;
